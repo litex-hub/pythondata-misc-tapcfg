407a5452361784d63f39ff1929d356eddefcef9f
LiteX Robot <robot@timvideos.us>
2022-05-25 20:17:12 +0000

    Updating pythondata-misc-tapcfg to 0.0.post506
    Updated data to v0.0-375-gbd557ff based on bd557ff00d8fe2473fcf346e36c96d004e94b8ca from https://github.com/enjoy-digital/tapcfg.
    > commit bd557ff00d8fe2473fcf346e36c96d004e94b8ca
    > Merge: 4ce399d 1a2accc
    > Author: enjoy-digital <florent@enjoy-digital.fr>
    > Date:   Wed May 1 12:11:43 2019 +0200
    >
    >     Merge pull request #1 from gsomlo/gls-strncpy-werror
    >
    >     fix strncpy string truncation warning/error
    >
    
    Updated using 0.0.post131 from https://github.com/litex-hub/litex-data-auto

--- pythondata_misc_tapcfg/__init__.py
+++ pythondata_misc_tapcfg/__init__.py
@@ -4,11 +4,11 @@ data_location = os.path.join(__dir__, "data")
 src = "https://github.com/enjoy-digital/tapcfg"
 
 # Module version
-version_str = "0.0.post503"
-version_tuple = (0, 0, 503)
+version_str = "0.0.post506"
+version_tuple = (0, 0, 506)
 try:
     from packaging.version import Version as V
-    pversion = V("0.0.post503")
+    pversion = V("0.0.post506")
 except ImportError:
     pass
 
@@ -35,11 +35,11 @@ Date:   Wed May 1 12:11:43 2019 +0200
 """
 
 # Tool version info
-tool_version_str = "0.0.post128"
-tool_version_tuple = (0, 0, 128)
+tool_version_str = "0.0.post131"
+tool_version_tuple = (0, 0, 131)
 try:
     from packaging.version import Version as V
-    ptool_version = V("0.0.post128")
+    ptool_version = V("0.0.post131")
 except ImportError:
     pass
 
